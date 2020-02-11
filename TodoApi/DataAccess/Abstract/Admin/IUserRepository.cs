d166190898e0dd23f9aa4243edea7d4ad576cb78
akhilnc <akhilnc.94@gmail.com>
2020-02-11 16:08:45 +0530

    cached data remove

--- TodoApi/DataAccess/Abstract/Admin/IUserRepository.cs
+++
@@ -1,8 +0,0 @@
-using System.Threading.Tasks;
-namespace TodoApi.DataAccess.Abstract.Admin
-{
-    public interface IUserRepository
-    {
-        Task<string> getUser();
-    }
-}
--- TodoApi/DataAccess/Abstract/DBHelper/IDbConnectionFactory.cs
+++
@@ -1,8 +0,0 @@
-using System.Data;
-namespace TodoApi.DataAccess.Abstract.DBHelper
-{
-    public interface IDbConnectionFactory
-    {
-          IDbConnection CreateConnection();
-    }
-}
